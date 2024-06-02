08b95e2d0be9e129a9f4122d82ffce8ca3fc8f9e
Peter deHaan <peter@deseloper.com>
2024-06-02 13:49:55 -0700

    Refactor into functions

--- index.js
+++ index.js
@@ -2,34 +2,43 @@ import fs from "node:fs/promises";
 import _chunk from "lodash.chunk";
 import _groupBy from "lodash.groupby";
 
-const res = await fs.readFile("./stats.txt", "utf-8");
-const lines = res.split("\n").filter((line) => line.trim().length);
+const lines = await loadEmojis("./stats.txt");
+const leaderboard = parseLeaderboard(lines, 10);
 
-const emojis = [];
-
-// Split into chunks of 4 lines...
-for (let [, emoji, date, name] of _chunk(lines, 4)) {
-  // Remove the fancy date suffixes.
-  date = date.replace(/(\d+)(st|nd|rd|th)/, "$1");
-  // Add in the missing current year.
-  if (!date.match(/\s\d{4}$/)) {
-    date += `, ${new Date().getFullYear()}`;
-  }
-  date = new Date(date);
-  emojis.push({ emoji, date, name });
+for (const row of leaderboard) {
+  console.log(`${row.name}: ${row.count}`);
 }
 
-// Group emojis by uploader, reduce into {name, count} pairs,
-// sort by highest count first, grab the top 10 results.
-let grouped = _groupBy(emojis, "name");
-grouped = Object.entries(grouped)
-  .reduce((acc, [k, v]) => {
-    acc.push({ name: k, count: v.length });
-    return acc;
-  }, [])
-  .sort((a, b) => b.count - a.count)
-  .slice(0, 10);
+async function loadEmojis(file = "./stats.txt") {
+  const res = await fs.readFile(file, "utf-8");
+  // Remove any blank lines
+  return res.trim().split("\n").filter((line) => line.trim().length);
+}
 
-for (const row of grouped) {
-  console.log(`${row.name}: ${row.count}`);
+function parseLeaderboard(lines = [], count = 10) {
+  const CURR_YEAR = new Date().getFullYear();
+  const emojis = [];
+
+  // Split into chunks of 4 lines...
+  for (let [, emoji, date, name] of _chunk(lines, 4)) {
+    // Remove the fancy date suffixes since it breaks `new Date()`.
+    date = date.replace(/(\d+)(st|nd|rd|th)/, "$1");
+    // Add in the missing current year.
+    if (!date.match(/\s\d{4}$/)) {
+      date += `, ${CURR_YEAR}`;
+    }
+    date = new Date(date);
+    emojis.push({ emoji, date, name });
+  }
+
+  // Group emojis by uploader, reduce into {name, count} pairs,
+  // sort by highest count first, grab the top 10 results.
+  const grouped = _groupBy(emojis, "name");
+  return Object.entries(grouped)
+    .reduce((acc, [k, v]) => {
+      acc.push({ name: k, count: v.length });
+      return acc;
+    }, [])
+    .sort((a, b) => b.count - a.count)
+    .slice(0, count);
 }
